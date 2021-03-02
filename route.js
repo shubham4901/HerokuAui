4102c79caf4b6a597f1ab2a2904f0005041d43ad
Shubham Vyas <svyas878@gmail.com>
2021-03-02 12:59:12 +0530

    bug fixed

--- route.js
+++ route.js
@@ -53,27 +53,4 @@ router.post("/register", async (req, res) => {
     await user.save();
     res.send("Registerd Successfully");
 });
-// router.delete('/books/:id', async (req, res) => {
-//     try {
-//         await Book.deleteOne({ _id: req.params.id });
-//         res.send("Record Deleted");
-//     } catch (error) {
-//         res.status(404).send({ error: "Book Not Found" });
-//     }
-// });
-// router.get("/books", async (req, res) => {
-//     const book = await Book.find();
-//     res.send(book);
-// });
-// router.patch('/books/:id', async (req, res) => {
-//     try {
-//         const book = await Book.findOne({ _id: req.params.id });
-//         book.name = req.body.name;
-//         book.qty = req.body.qty;
-//         await book.save();
-//         res.send(book);
-//     } catch (error) {
-//         res.status(404).send(error);
-//     }
-// });
 module.exports = router;
